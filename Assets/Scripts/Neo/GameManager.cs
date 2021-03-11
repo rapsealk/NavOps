8e1c8bae449bbf81b0e24c8d751273415565cb07
rapsealk <piono623@naver.com>
2021-03-11 17:29:52 +0900

    WIP: Frame Rate = 60으로 설정

--- Assets/Scripts/Neo/GameManager.cs
+++ Assets/Scripts/Neo/GameManager.cs
@@ -27,7 +27,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Application.targetFrameRate = 60;
     }
 
     // Update is called once per frame
